0a0ee4f5e6159a70ddb388669b594124f69883de
raesga02 <pabloraesga@gmail.com>
2024-10-28 18:39:17 +0100

    SAT test on CPU with optimised detection for collisions

--- Assets/Scripts/TestSAT.cs
+++ Assets/Scripts/TestSAT.cs
@@ -9,34 +9,28 @@ public class TestSAT : MonoBehaviour
     public Vector2[] vertex;
     public bool overlap;
     public float damping = 1f;
+    public BounceDirection bounceDir = BounceDirection.INSIDE;
 
 
     private void OnDrawGizmos() {
         vertex = new Vector2[4] {
-            transform.position + new Vector3(  size.x / 2 + 1,   size.y / 2, 0f),
+            transform.position + new Vector3(  size.x / 2 + 5,   size.y / 2, 0f),
             transform.position + new Vector3(- size.x / 2 - 1,   size.y / 2 - 1, 0f),
             transform.position + new Vector3(- size.x / 2, - size.y / 2, 0f),
             transform.position + new Vector3(  size.x / 2, - size.y / 2, 0f)
         };
 
-        Vector2[] points = new Vector2[2] {
-            point.transform.position - new Vector3(point.radius, point.radius, 0),
-            point.transform.position + new Vector3(point.radius, point.radius, 0)
-        };
-
-        overlap = Collision.CheckOverlap(points, vertex);
+        // Get position to move to to resolve
+        Vector2 moveVector = Collision.SATResponse(point.transform.position, vertex, point.radius);
+        overlap = !moveVector.Equals(Vector2.zero);
 
         DrawPolygon(overlap ? Color.red : Color.white);
 
-
-        // Get position to move to to resolve
         if (overlap) {
-            Vector2 moveVector = Collision.MTVBetween(points, vertex);
-
             // Draw moved particle
             Vector2 movedPoint = point.transform.position + new Vector3(moveVector.x, moveVector.y, 0);
             Gizmos.color = Color.cyan;
-            Gizmos.DrawWireSphere(movedPoint, 1f);
+            Gizmos.DrawWireSphere(movedPoint, point.radius);
 
             // Draw move normal
             Gizmos.color = Color.gray;
@@ -78,15 +72,15 @@ public static class Collision
         return projectionLine;
     }
 
-    private static ProjectionLine ProjectLine(Vector2 point, Vector2 normal) {
-        float projectionScale = Vector2.Dot(point, normal);
-        return new ProjectionLine() { start = projectionScale, end = projectionScale };
+    private static ProjectionLine ProjectPoint(Vector2 point, float radius, Vector2 normal) {
+        float projectedPoint = Vector2.Dot(point, normal);
+        return new ProjectionLine() { start = projectedPoint - radius, end = projectedPoint + radius };
     }
 
-    public static bool CheckOverlap(Vector2[] point, Vector2[] collider) {
+    public static bool CheckOverlap(Vector2 point, float radius, Vector2[] collider) {
         for (int i = 0; i < collider.Length; i++) {
             Vector2 edgeNormal = NormalBetween(collider[i], collider[(i + 1) % collider.Length]);
-            ProjectionLine pointProjection = ProjectLine(point, edgeNormal);
+            ProjectionLine pointProjection = ProjectPoint(point, radius, edgeNormal);
             ProjectionLine colliderProjection = ProjectLine(collider, edgeNormal);
 
             if (!(pointProjection.start <= colliderProjection.end && pointProjection.end >= colliderProjection.start)) {
@@ -99,16 +93,17 @@ public static class Collision
 
     private static float? CollisionResponseAcrossLine(ProjectionLine pointProjection, ProjectionLine colliderProjection)
     {
+        // If point completely inside of collider
         if (pointProjection.start <= colliderProjection.start && pointProjection.end >= colliderProjection.start)
             return colliderProjection.start - colliderProjection.end;
+        // If point partially inside
         else if (colliderProjection.start <= pointProjection.start && colliderProjection.end >= pointProjection.start)
             return colliderProjection.end - pointProjection.start;
         return null;
     }
 
-    public static Vector2 MTVBetween(Vector2[] point, Vector2[] collider)
-    {
-        if (!CheckOverlap(point, collider)) { return Vector2.zero; }
+    public static Vector2 MTVBetween(Vector2 point, Vector2[] collider, float radius) {
+        if (!CheckOverlap(point, radius, collider)) { return Vector2.zero; }
 
         float minResponseMagnitude = float.MaxValue;
         Vector2 responseNormal = Vector2.zero;
@@ -116,7 +111,7 @@ public static class Collision
         for (int c = 0; c < collider.Length; c++)
         {
             Vector2 cEdgeNormal = NormalBetween(collider[c], collider[(c + 1) % collider.Length]);
-            ProjectionLine pointProjected = ProjectLine(point, cEdgeNormal);
+            ProjectionLine pointProjected = ProjectPoint(point, radius, cEdgeNormal);
             ProjectionLine colliderProjected = ProjectLine(collider, cEdgeNormal);
 
             float? responseMagnitude = CollisionResponseAcrossLine(pointProjected, colliderProjected);
@@ -127,6 +122,34 @@ public static class Collision
             }
         }
 
-        return responseNormal * Mathf.Abs(minResponseMagnitude);
+        return responseNormal * minResponseMagnitude;
+    }
+
+    public static Vector2 SATResponse(Vector2 point, Vector2[] collider, float radius) {
+        float minResponseMagnitude = float.MaxValue;
+        Vector2 responseNormal = Vector2.zero;
+
+        for (int i = 0; i < collider.Length; i++) {
+            Vector2 edgeNormal = NormalBetween(collider[i], collider[(i + 1) % collider.Length]);
+            ProjectionLine pointProjection = ProjectPoint(point, radius, edgeNormal);
+            ProjectionLine colliderProjection = ProjectLine(collider, edgeNormal);
+
+            float? responseMagnitude = CollisionResponseAcrossLine(pointProjection, colliderProjection);
+
+            // If not overlap return Vector2.zero as response
+            if (!(pointProjection.start <= colliderProjection.end && pointProjection.end >= colliderProjection.start)) {
+                return Vector2.zero;
+            }
+            // If candidate to collide annotate the min magnitude
+            else {
+                if (Mathf.Abs((float)responseMagnitude) < Mathf.Abs((float)minResponseMagnitude)) {
+                    minResponseMagnitude = (float) responseMagnitude;
+                    responseNormal = edgeNormal;
+                }
+            }
+        }
+
+        // If loop ends and not found false then overlaps, return response found
+        return responseNormal * minResponseMagnitude;
     }
 }
