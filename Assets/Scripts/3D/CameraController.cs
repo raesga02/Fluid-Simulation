2d08db3a66268e8ca282eb3e32cc234914c46c47
raesga02 <pabloraesga@gmail.com>
2024-12-11 17:36:22 +0100

    rotation and cursor hiding

--- Assets/Scripts/3D/CameraController.cs
+++ Assets/Scripts/3D/CameraController.cs
@@ -5,7 +5,11 @@ public class CameraController : MonoBehaviour {
     [Header("Movement Control")]
     public Vector3 targetPosition;
     public Vector3 velocity = Vector3.zero;
-    public float maxSpeed = 1.0f;
+
+    [Header("Rotation Control")]
+    public float mouseSensitivity = 100f;
+    public float xRotation = 0f;
+    public float yRotation = 0f;
 
     private void Start() {
         targetPosition = transform.localPosition;
@@ -21,5 +25,28 @@ public class CameraController : MonoBehaviour {
         if (Input.GetKey("left shift")) { targetPosition -= transform.up; }
 
         transform.localPosition = Vector3.SmoothDamp(transform.localPosition, targetPosition, ref velocity, 0.25f);
+
+        // Rotation
+        if (Input.GetKey("mouse 1") || true) {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            yRotation += mouseX;
+            
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        }
+
+        // Cursor show
+        if (Input.GetKeyDown("escape")) {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        // Cursor hide
+        if (Input.GetMouseButtonDown(0)) {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
 }
