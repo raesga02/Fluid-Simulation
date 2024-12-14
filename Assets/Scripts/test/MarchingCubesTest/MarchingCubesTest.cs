6d7ab3e3d7d687ffab046d2ba4c405ae118bbebd
raesga02 <pabloraesga@gmail.com>
2024-12-14 14:03:21 +0100

    added debug visuals, normal calculation

--- Assets/Scripts/test/MarchingCubesTest/MarchingCubesTest.cs
+++ Assets/Scripts/test/MarchingCubesTest/MarchingCubesTest.cs
@@ -57,6 +57,14 @@ public class MarchingCubesTest : MonoBehaviour {
     // Private fields
     bool needsUpdate = true;
 
+    // DEBUG
+    [Header("DEBUG")]
+    public uint verticesAdded;
+    public Vector3[] vertPoss;
+    public Vector3[] normDir;
+    public int[] triangIdx;
+    public bool draw = false;
+
 
     void Start() {
         Init();
@@ -65,13 +73,18 @@ public class MarchingCubesTest : MonoBehaviour {
     void Init() {
         UpdateSettings();
 
-        samplesInitialData = Enumerable.Range(0, samplesPerAxis * samplesPerAxis * samplesPerAxis).Select(idx => {
-            int n = samplesPerAxis * samplesPerAxis * samplesPerAxis;
-            int x = idx % n;
-            int y = (idx / n) % n;
-            int z = (idx / (n * n)) % n;
-            return new Sample() { position = new Vector3(x, y, z), value = 0.0f };
-        }).ToArray();
+        samplesInitialData = new Sample[samplesPerAxis * samplesPerAxis * samplesPerAxis];
+        Vector3 spacing = bounds / (samplesPerAxis - 1);
+        Vector3 origin = transform.position - bounds * 0.5f;
+        for (int z = 0; z < samplesPerAxis; z++) {
+            for (int y = 0; y < samplesPerAxis; y++) {
+                for (int x = 0; x < samplesPerAxis; x++) {
+                    int sampleIdx = x + y * samplesPerAxis + z * samplesPerAxis * samplesPerAxis;
+                    Vector3 samplePos = origin + Vector3.Scale(new Vector3Int(x, y, z), spacing);
+                    samplesInitialData[sampleIdx] = new Sample() { position = samplePos, value = 0.0f };
+                }
+            }
+        }
         verticesInitialData = Enumerable.Repeat(new Vertex() { position = Vector3.zero, normal = Vector3.zero }, maxNumTriangles * 3).ToArray();
         verticesCounterInitialData = new uint[1] { 0 };
         trianglesInitialData = Enumerable.Repeat(0, maxNumTriangles * 3).ToArray();
@@ -92,7 +105,7 @@ public class MarchingCubesTest : MonoBehaviour {
         samplesBuffer = new ComputeBuffer(samplesPerAxis * samplesPerAxis * samplesPerAxis, Marshal.SizeOf(typeof(Sample)));
         verticesBuffer = new ComputeBuffer(maxNumTriangles * 3, sizeof(float) * 6);
         verticesCounterBuffer = new ComputeBuffer(1, sizeof(uint), ComputeBufferType.Counter);
-        trianglesBuffer = new ComputeBuffer(maxNumTriangles * 3, sizeof(int) * 3);
+        trianglesBuffer = new ComputeBuffer(maxNumTriangles * 3, sizeof(int), ComputeBufferType.Append);
     }
 
     void FillComputeBuffers() {
@@ -124,8 +137,9 @@ public class MarchingCubesTest : MonoBehaviour {
         Vector3Int numGroupsSample = GraphicsHelper.ComputeThreadGroups3D(samplesPerAxis, samplesPerAxis, samplesPerAxis, Vector3Int.one * 8);
         computeShader.Dispatch(sampleDensitiesKernel, numGroupsSample.x, numGroupsSample.y, numGroupsSample.z);
 
-        // Reset the counter
+        // Reset the counters
         computeShader.Dispatch(resetCounterKernel, 1, 1, 1);
+        trianglesBuffer.SetCounterValue(0);
 
         // March the cubes
         Vector3Int numGroupsMarch = GraphicsHelper.ComputeThreadGroups3D(samplesPerAxis - 1, samplesPerAxis - 1, samplesPerAxis - 1, Vector3Int.one * 8);
@@ -135,9 +149,16 @@ public class MarchingCubesTest : MonoBehaviour {
         // TODO: change with dispatch kernel that modifies args buffer
         uint[] numVerticesCounter = new uint[1];
         verticesCounterBuffer.GetData(numVerticesCounter);
-        Debug.Log(numVerticesCounter[0]);
+        verticesAdded = numVerticesCounter[0];
+
+        verticesBuffer.GetData(verticesInitialData);
+        vertPoss = verticesInitialData.Select(vertex => vertex.position).ToArray();
+        normDir = verticesInitialData.Select(vertex => vertex.normal).ToArray();
+        trianglesBuffer.GetData(trianglesInitialData);
+        triangIdx = trianglesInitialData;
 
         // TODO: Draw the mesh
+        draw = true;
     }
 
     void UpdateSettings() {
@@ -162,6 +183,18 @@ public class MarchingCubesTest : MonoBehaviour {
     private void OnDrawGizmos() {
         if (drawBounds) { DrawBounds(); }
         if (drawSamplePoints) { DrawSamplePoints(); }
+
+        if (draw) {
+            for (int i = 0; i < verticesAdded; i+= 3) {
+                Vector3[] triangleVertices = vertPoss.Skip(i).Take(3).ToArray();
+                Vector3 normal = normDir[i];
+                Vector3 avgPos = triangleVertices.Aggregate(Vector3.zero, (acc, v) => acc + v) / 3;
+                Gizmos.color = new Color(0.35f, 0.65f, 0.85f);
+                Gizmos.DrawLineStrip(triangleVertices, true);
+                Gizmos.color = new Color(normal.x, normal.y, normal.z, 1.0f);
+                Gizmos.DrawLine(avgPos, avgPos + normal * 0.2f);
+            }
+        }
     }
 
     void DrawBounds() {
@@ -177,7 +210,7 @@ public class MarchingCubesTest : MonoBehaviour {
             for (int j = 0; j < samplesPerAxis; j++) {
                 for (int k = 0; k < samplesPerAxis; k++) {
                     Vector3 samplePos = origin + Vector3.Scale(new Vector3Int(i, j, k), spacing);
-                    Gizmos.DrawCube(samplePos, Vector3.one * 0.2f);
+                    Gizmos.DrawCube(samplePos, Vector3.one * 0.05f);
                 }
             }
         }
